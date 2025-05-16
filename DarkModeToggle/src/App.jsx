1587ed976bbb63b68d4671dfc4bbe80b256c3862
GUNNYTHATIPAMULA <139752490+GUNNYTHATIPAMULA@users.noreply.github.com>
2025-05-16 20:35:44 +0530

    themeswitch (#3)
    * themeswitch
    
    * themeswitch

---
+++ DarkModeToggle/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+
+function App() {
+ const [background,setBackground] = useState('bg-white')
+  const [textColor,setTextColor] = useState('text-black')
+  const [text, setText] = useState('Dark')
+ const toggleBackground = () => {
+  if (background === 'bg-white') {
+    setBackground('bg-black')
+    setText('Light')
+    setTextColor('text-white')
+  } else {
+    setBackground('bg-white')
+    setText('Dark')
+    setTextColor('text-black')
+  }
+ }
+
+  return (
+    <>
+     <div className={`w-full h-screen flex items-center justify-center ${background} ` }>
+      <button onClick={toggleBackground} className={` bg-blue-500 p-3 rounded-full text-white`} >{text}mode</button>
+     </div>
+    </>
+  )
+}
+
+export default App
